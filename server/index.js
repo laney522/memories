a431fda09452fb05772dcb339f87cb9bd2defe8b
laney522 <pm72222@gmail.com>
2023-02-09 22:20:10 +0900

    update

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-import express from 'express'; 
-import bodyParser from 'body-parser'; 
-import mongoose from 'mongoose'; 
-import cors from 'cors'; 
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-const app = express(); 
+const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true })); 
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); 
-app.use(cors()); 
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 
-const CONNECTION_URL = 'mongodb+srv://laney:laney@cluster0.01b9egk.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://laney522:laney522@cluster0.01b9egk.mongodb.net/?retryWrites=true&w=majority';
+const POST = process.env.PORT || 5000;
 
 // https://www.mongodb.com/cloud/atlas 
