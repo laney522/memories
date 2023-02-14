5176cdc0aeac260fcec85bd244423b0f6d42c414
laney522 <pm72222@gmail.com>
2023-02-14 22:59:26 +0900

    update controllers>posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,17 @@
+import PostMessage from '../models/postMessage.js';
 
-export const getPosts = (req, res) => {
-  res.send('THIS WORKS!');
+export const getPosts = async (req, res) => {
+  try {
+    const postMessage = await PostMessage.find();
+
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    
+  }
+}
+
+export const createPost = (req, res) => {
+  res.send('Post Creation');
 }
