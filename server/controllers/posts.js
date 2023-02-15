cf8237df32249adc91d0ca4f74c758fb1fabe476
laney522 <pm72222@gmail.com>
2023-02-15 23:39:02 +0900

    update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,10 +8,10 @@ export const getPosts = async (req, res) => {
 
     res.status(200).json(postMessages);
   } catch (error) {
-    
+    res.status(404).json({ message: error.message });
   }
 }
 
 export const createPost = (req, res) => {
-  res.send('Post Creation');
+    res.send('Post Creation');
 }
