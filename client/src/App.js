e30876b9a156530f0ad0361ac076dca22808c931
laney522 <pm72222@gmail.com>
2023-02-22 21:59:55 +0900

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import memories from './images/memories.png'
+
 const App = () => {
   return (
     <Container maxidth="lg">
@@ -8,6 +10,18 @@ const App = () => {
         <Typography variant='h2' align='center'>Memories</Typography>
         <img src={memories} alt="memories" height="60" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems='stretch' spacing={3}>
+              <Grid item xs={12} sm={7}>
+                  <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                  <Form />
+              </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
