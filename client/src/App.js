02e39bb4cdca4efb22944ce7775f69589ad6e8e0
laney522 <pm72222@gmail.com>
2023-02-23 22:01:53 +0900

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import memories from './images/memories.png'
 
 const App = () => {
@@ -13,12 +15,12 @@ const App = () => {
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems='stretch' spacing={3}>
-              <Grid item xs={12} sm={7}>
-                  <Posts />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                  <Form />
-              </Grid>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
           </Grid>
         </Container>
       </Grow>
