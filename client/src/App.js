8f8ba632c44aaa5b01ac1ee6bb5e4e23e7cb07c4
laney522 <pm72222@gmail.com>
2023-02-27 21:57:58 +0900

    update src>App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ const App = () => {
   return (
     <Container maxidth="lg">
       <AppBar className={classes.appBar} position='static' color="inherit">
-        <Typography variant='h2' align='center'>Memories</Typography>
-        <img src={memories} alt="memories" height="60" />
+        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>
