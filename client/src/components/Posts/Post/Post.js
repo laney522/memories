4b95a970007317f07c9dd882685af0364e83293f
laney522 <pm72222@gmail.com>
2023-03-02 22:36:23 +0900

    update

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -3,8 +3,9 @@ import React from 'react';
 import useStyles from './styles';
 
 const Post = () => {
+  // eslint-disable-next-line
   const classes = useStyles();
-  
+
   return (
     <h1>POST</h1>
   )
