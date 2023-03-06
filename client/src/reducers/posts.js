d091a9c66476b7cf8cfe205dcda1a71f3aa3e9b0
laney522 <pm72222@gmail.com>
2023-03-06 21:23:57 +0900

    update

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,10 @@
 const reducers = (posts = [], action) => {
-  switch (action.type) { 
-    case 'FETCH_ALL': 
-      return posts; 
-    case 'CREATE': 
-      return posts; 
-    default: 
-      break; 
-  } 
+  switch (action.type) {
+    case 'FETCH_ALL':
+      return posts;
+    case 'CREATE':
+      return posts;
+    default:
+      return posts;
+  }
 }
