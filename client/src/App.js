6630e7a4bbf9b89e4395ec7a74014893bd01391f
laney522 <pm72222@gmail.com>
2023-03-08 23:35:30 +0900

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
