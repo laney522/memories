d0e3a762712052bd12772fcc8af9f2c0a5119a90
laney522 <pm72222@gmail.com>
2023-03-10 21:27:05 +0900

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
-import Posts from './components/Posts/Posts'; 
-import Form from './components/Form/Form'; 
-import memories from './images/memories.png' 
-import useStyles from './styles'; 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png'
+import useStyles from './styles';
 
 const App = () => {
-  const classes = useStyles(); 
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+
+  }, []);
 
   return (
     <Container maxidth="lg">
