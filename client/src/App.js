f1be4bb6ccfca7ff84c95014f2c1fdb6d96e4394
laney522 <pm72222@gmail.com>
2023-03-11 21:11:43 +0900

    update

--- client/src/App.js
+++ client/src/App.js
@@ -20,13 +20,13 @@ const App = () => {
       <AppBar className={classes.appBar} position='static' color="inherit">
         <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
         <img className={classes.image} src={memories} alt="memories" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
+      </AppBar> 
+      <Grow in> 
+        <Container> 
           <Grid container justify="space-between" alignItems='stretch' spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts />
-            </Grid>
+              <Posts /> 
+            </Grid> 
             <Grid item xs={12} sm={4}>
               <Form />
             </Grid>
