16b0d0fd4cebdfa7470431ffd77bc1af9c60671e
laney522 <pm72222@gmail.com>
2023-03-17 22:30:51 +0900

    update

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,7 +7,7 @@ const Form = () => {
   const classes = useStyles();
   return (
     <h1>FORM</h1>
-  )
+  );
 }
 
 export default Form;
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
 
+import useStyles from './styles';
 const Posts = () => {
+  const classes = useStyles();
+
   return (
     <>
       <h1>POSTS</h1>
