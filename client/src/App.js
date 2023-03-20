4df2186dcb7763bdc345910c142f78730828f1a2
laney522 <pm72222@gmail.com>
2023-03-20 18:03:32 +0900

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png'
@@ -12,7 +13,7 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-      dispatch(getPosts());
+    dispatch(getPosts());
   }, [dispatch]);
 
   return (
@@ -20,13 +21,13 @@ const App = () => {
       <AppBar className={classes.appBar} position='static' color="inherit">
         <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
         <img className={classes.image} src={memories} alt="memories" height="60" />
-      </AppBar> 
-      <Grow in> 
-        <Container> 
+      </AppBar>
+      <Grow in>
+        <Container>
           <Grid container justify="space-between" alignItems='stretch' spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts /> 
-            </Grid> 
+              <Posts />
+            </Grid>
             <Grid item xs={12} sm={4}>
               <Form />
             </Grid>
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { configureStore, applyMiddleware, compose } from 'redux';
+import { applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { configureStore } from "./app/store";
 
-import { reducers } from '/reducers'; 
-import App from './App'; 
-import './index.css'; 
+import { reducers } from '/reducers';
+import App from './App';
+import './index.css';
 
-const store = configureStore(reducers, compose(applyMiddleware(thunk))); 
+const store = configureStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <Provider store={store}> 
-    <App /> 
-  </Provider>, 
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
