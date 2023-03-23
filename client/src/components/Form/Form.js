3cfa39e8291e316c047de155b24e7d216aa120a9
laney522 <pm72222@gmail.com>
2023-03-23 23:21:41 +0900

    update

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
 import useStyles from './styles';
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: '', title: '', message: '', tags: '', selectedFile: '',
+  });
   // eslint-disable-next-line
   const classes = useStyles();
 
   const handleSubmit = () => {
-    
+
   }
 
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
-
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          variant='outlined'
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={ }
+        />
       </form>
     </Paper>
   );
