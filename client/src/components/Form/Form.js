1b060f0018ed52d973cf7257084ae9a37bc51410
laney522 <pm72222@gmail.com>
2023-03-24 22:04:38 +0900

    update

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -24,7 +24,7 @@ const Form = () => {
           label="Creator"
           fullWidth
           value={postData.creator}
-          onChange={ }
+          onChange={(e) => setPostData({ ... postData, creator: e.target.value })}
         />
       </form>
     </Paper>
